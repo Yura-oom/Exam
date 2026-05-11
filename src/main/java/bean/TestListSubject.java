956371427d60efda5be3b81f965f0e5ba9809207
t_ohara <t_ohara@LAPTOP-U34PG4BF>
2026-05-11 12:11:14 +0900

    直したファイルです

--- src/main/java/bean/TestListSubject.java
+++ src/main/java/bean/TestListSubject.java
@@ -1,61 +1,66 @@
 package bean;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class TestListSubject implements java.io.Serializable {
-	
+
 	private String classNum;
 	private int entYear;
 	private String studentName;
 	private String studentNo;
-	private Map<Integer, Integer> points;
-	
-	public int getEntYear;
-	public String getStudentNo() {
-		return studentNo;
-	
-	}
-	
-	public String getStudentName() {
-		return studentName;
-	
-	}
-	
+	private Map<Integer, Integer> points = new HashMap<>();
+
 	public String getClassNum() {
 		return classNum;
-	
 	}
-	
-	public String getPoint(int key) {
-		return;
-	
+
+	public void setClassNum(String classNum) {
+		this.classNum = classNum;
 	}
-	
-	public Map<Integer, Integer> getPoints(){
-		return null;
-	
+
+	public int getEntYear() {
+		return entYear;
 	}
-	
+
 	public void setEntYear(int entYear) {
 		this.entYear = entYear;
-	
 	}
-	
-	public void setStudentName(String classNum) {
-		return;
-	
+
+	public String getStudentName() {
+		return studentName;
 	}
-	
-	public void setClassNum(String classNum) {
-		return;
-	
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
 	}
-	
-	public void putPoint(int key, int value) {
-		
+
+	public String getStudentNo() {
+		return studentNo;
 	}
-	
-	public void setPoints(Map<Integer, Integer>) {
+
+	public void setStudentNo(String studentNo) {
+		this.studentNo = studentNo;
+	}
+
+	public Map<Integer, Integer> getPoints() {
+		return points;
+	}
+
+	public void setPoints(Map<Integer, Integer> points) {
 		this.points = points;
 	}
+
+	public String getPoint(int key) {
+		Integer value = points.get(key);
+		if (value == null) {
+			return "";
+			
+		}
+		return String.valueOf(value);
 	}
+
+	public void putPoint(int key, int value) {
+		points.put(key, value);
+	}
+}
